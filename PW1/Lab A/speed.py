7375c615842a7ca792b2b2d9af60994f70e67bad
Sahib Farzaliyev <s.farzaliyev@ufaz.az>
2026-09-18 03:55:08 +0400

    Added speed.py, a program that checks how much faster the fast version is

---
+++ PW1/Lab A/speed.py
@@ -0,0 +1,24 @@
+from decay import simulate, simulate_loop
+import time
+import numpy as np # simulate function needs numpy, not sure if I need to import it in this file also, did it just in case
+
+start = time.perf_counter()
+
+for i in range(100):
+    simulate(10000, 0.1, seed = i)
+
+end = time.perf_counter()
+
+fast = end - start
+
+
+start = time.perf_counter()
+
+for i in range(100):
+    simulate_loop(10000, 0.1, seed = i)
+
+end = time.perf_counter()
+
+slow = end - start
+
+print("The fast vectorized version is " + str(slow / fast) + " times faster than the slow, loop based version.")
